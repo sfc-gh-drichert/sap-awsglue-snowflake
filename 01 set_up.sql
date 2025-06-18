0b5c7746b21d64bffc920d52dc697f022a7f8b0f
David Richert <68942370+sfc-gh-drichert@users.noreply.github.com>
2025-06-18 10:19:51 +0200

    commented out role creation

--- 01 set_up.sql
+++ 01 set_up.sql
@@ -2,16 +2,25 @@
 Author: David Richert
 Date: 26 May 2025
 Documentation: https://docs.snowflake.com/en/user-guide/data-load-s3-config-storage-integration
+ 
+ 
+ 
+ Use this code to set up the roles, S3 to Snowflake integration, database, schemas, table definitions, 
+ and snowpipe pipelines to ingest data from AWS S3 buckets.
+ 
+ Please run the AWS Glue job before executing this code. This will enable Snowflake to build the tables from the parquet files.
+
  */
 USE ROLE ACCOUNTADMIN;
 SET saps3targeturl = 's3://sap-s3-raw/s4/fi/ar/'; --this is the directory where you will land the files from AWS glue (minus the name of the extractor)
 --CREATE sap role and assign grants to create and own the rest of the objects
-CREATE OR REPLACE ROLE SAP_ROLE;
-GRANT CREATE DATABASE ON ACCOUNT TO ROLE SAP_ROLE; --grant so the role can creat the database.
-GRANT CREATE INTEGRATION ON ACCOUNT TO ROLE SAP_ROLE; --grant to create the integration between Snowflake and AWS S3.
-GRANT ROLE SAP_ROLE to ROLE accountadmin; --ASSIGN ROLE TO an existing role that your user has.
-USE ROLE SAP_ROLE;
+--CREATE OR REPLACE ROLE SAP_ROLE;
+--GRANT CREATE DATABASE ON ACCOUNT TO ROLE SAP_ROLE; --grant so the role can creat the database.
+--GRANT CREATE INTEGRATION ON ACCOUNT TO ROLE SAP_ROLE; --grant to create the integration between Snowflake and AWS S3.
+--GRANT ROLE SAP_ROLE to ROLE accountadmin; --ASSIGN ROLE TO an existing role that your user has.
+--USE ROLE SAP_ROLE;
 
+--Use the following to set up and validate the integration between Snowflake and S3 buckets. Check out the documentation on how to get the ARN and external ID.
 CREATE OR REPLACE STORAGE INTEGRATION SAP_INTEGRATION --more infor here https://docs.snowflake.com/en/user-guide/data-load-s3-config-storage-integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
@@ -19,32 +28,33 @@ CREATE OR REPLACE STORAGE INTEGRATION SAP_INTEGRATION --more infor here https://
   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::631484165566:role/sap_snowflake_s3_access_role' --obfuscate for quickstart
   STORAGE_AWS_EXTERNAL_ID = 'EU_DEMO47_SFCRole=3571_xQgnqahg8fymSujM2bXWqAWHwOM=' --so you don't have to set it each time your recreate the storage integration.
   STORAGE_ALLOWED_LOCATIONS = ('s3://sap-s3-raw/');
-SHOW  STORAGE INTEGRATIONS;
-DESC INTEGRATION SAP_INTEGRATION;
-SELECT SYSTEM$VALIDATE_STORAGE_INTEGRATION ('SAP_INTEGRATION', 's3://sap-s3-raw/', 'validate_all.txt', 'all');
-CREATE or REPLACE DATABASE SAP_RAW_AWS COMMENT = 'For S4 raw data';
-USE DATABASE SAP_RAW_AWS; --change to SAP_LLM_ANALYST2
-CREATE OR REPLACE SCHEMA SAP_RAW_AWS.FI; --change to SAP_RAW
-USE SCHEMA SAP_RAW_AWS.FI;
---Create file format
+
+--Validate the storage integration
+--SHOW  STORAGE INTEGRATIONS;
+--DESC INTEGRATION SAP_INTEGRATION;
+--SELECT SYSTEM$VALIDATE_STORAGE_INTEGRATION ('SAP_INTEGRATION', 's3://sap-s3-raw/', 'validate_all.txt', 'all');
+
+--Create a database and schema to land the raw data from SA 
+CREATE or REPLACE DATABASE SAP_SNOW COMMENT = 'For S4 raw data';
+USE DATABASE SAP_SNOW; 
+CREATE OR REPLACE SCHEMA SAP_RAW; 
+USE SCHEMA  SAP_RAW;
+--Create file format so Snowflake knows that the SAP files are coming into the S3 bucket as parquet files.
 CREATE OR REPLACE FILE FORMAT SAP_FILE TYPE = parquet COMMENT = 'Parquet file format for AWS Glue files landed in S3';
---create stage
+
+--create a Snowflake stage to point to the S3 bucket.
 CREATE OR REPLACE STAGE SAP_STAGE
     URL = $SAPS3TARGETURL
     STORAGE_INTEGRATION = SAP_INTEGRATION
     FILE_FORMAT = SAP_FILE;
 
   --Test integration
-  --run the job from AWS glue to get files to work with
- SHOW stages;
-LIST @SAP_STAGE/fi_ar_4/; --copy one of the file names from the result.
-LIST @SAP_STAGE/customer;
-LIST @SAP_STAGE/material/;
+  --run the job from AWS glue to get files to work with. Check out the cloud formation template to get this. 
+ --SHOW stages; LIST @SAP_STAGE/fi_ar_4/; LIST @SAP_STAGE/customer; LIST @SAP_STAGE/material/;
 
+--Let's use Snowflake's inference capability to build up the table definitions for the raw layer.
 
---Create DDLs 
-
-  CREATE OR REPLACE TABLE FI_AR_4 --change to "0fi_ar_4"
+ CREATE OR REPLACE TABLE FI_AR_4 --change to "0fi_ar_4"
   USING TEMPLATE (
     SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
       FROM TABLE(
@@ -54,7 +64,7 @@ LIST @SAP_STAGE/material/;
         )
       ));
 
-SELECT get_ddl('TABLE', 'Z_FI_AR_4');
+--SELECT get_ddl('TABLE', 'Z_FI_AR_4');
 
   CREATE OR REPLACE TABLE CUSTOMER
   USING TEMPLATE (
@@ -78,7 +88,7 @@ CREATE OR REPLACE TABLE MATERIAL
         )
       ));
 
-SELECT get_ddl('TABLE', 'MATERIAL');
+--SELECT get_ddl('TABLE', 'MATERIAL');
 
 
 --!!now delete the files from S3!!
@@ -86,9 +96,7 @@ REMOVE @sap_stage/fi_ar_4/;
 REMOVE @sap_stage/customer/;
 REMOVE @sap_stage/material/;
 --truncate tables
-TRUNCATE TABLE FI_AR_4;
-TRUNCATE TABLE CUSTOMER;
-TRUNCATE TABLE MATERIAL;
+TRUNCATE TABLE FI_AR_4; TRUNCATE TABLE CUSTOMER; TRUNCATE TABLE MATERIAL;
 
 CREATE OR REPLACE PIPE FI_AR_4_PIPE
 AUTO_INGEST = TRUE
@@ -116,29 +124,25 @@ AS
 
 SHOW pipes; -- get the ARN and configure the SNS for the S3 bucket from the AWS console
 /*arn:aws:sqs:us-west-2:227202484309:sf-snowpipe-AIDATJZSSZBKXUQTZVL4W-j9b6KZYM9aqg3IToJ1ngqQ */
-SELECT SYSTEM$PIPE_STATUS ('FI_AR_4_PIPE');
-SELECT SYSTEM$PIPE_STATUS ('CUSTOMER_PIPE');
-SELECT SYSTEM$PIPE_STATUS ('MATERIAL_PIPE');
+SELECT SYSTEM$PIPE_STATUS ('FI_AR_4_PIPE'); SELECT SYSTEM$PIPE_STATUS ('CUSTOMER_PIPE'); SELECT SYSTEM$PIPE_STATUS ('MATERIAL_PIPE');
   --ALTER PIPE Z_BW_ODATA_0FI_AR_4_1_SRV_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
   
-  --RUN  THE AWS GLUE JOB NOW!è
-  --after its executed, but before the SNS has kicked in  (can take minutes) check that the files have landed in the S3 bucket:
-LIST @SAP_STAGE/fi_ar_4/; --copy one of the file names from the result.
-LIST @SAP_STAGE/customer/;
-LIST @SAP_STAGE/material/;
-SELECT * FROM FI_AR_4; --5201 rows
-SELECT * FROM CUSTOMER; --201
-SELECT *  FROM MATERIAL; --2701
+/*
 
----TO PAUSE!!!!!!!!
---ALTER PIPE FI_AR_4_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
---ALTER PIPE CUSTOMER_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
---ALTER PIPE MATERIAL_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+RE-RUN  THE AWS GLUE JOB NOW! The pipes need to be in a running state.
 
-  /* Clean up -- Deletes database 
-Author: David Richert
-Date: 9 May 2025
 */
 
+LIST @SAP_STAGE/fi_ar_4/; LIST @SAP_STAGE/customer/; LIST @SAP_STAGE/material/;
+
+SELECT * FROM FI_AR_4; SELECT * FROM CUSTOMER; SELECT *  FROM MATERIAL; --2701
+
+---TO PAUSE!!!!!!!!
+ALTER PIPE FI_AR_4_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+ALTER PIPE CUSTOMER_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+ALTER PIPE MATERIAL_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+
 --Tear down
---DROP DATABASE SAP_RAW_AWS;
+--DROP DATABASE   SAP_SNOW;
+
+
