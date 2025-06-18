31f8904087d189e470fd80c8ab9736dad0afe11f
David Richert <68942370+sfc-gh-drichert@users.noreply.github.com>
2025-06-18 11:08:47 +0200

    added comments

--- 01 set_up.sql
+++ 01 set_up.sql
@@ -13,15 +13,18 @@ Documentation: https://docs.snowflake.com/en/user-guide/data-load-s3-config-stor
  */
 USE ROLE ACCOUNTADMIN;
 SET saps3targeturl = 's3://sap-s3-raw/s4/fi/ar/'; --this is the directory where you will land the files from AWS glue (minus the name of the extractor)
---CREATE sap role and assign grants to create and own the rest of the objects
---CREATE OR REPLACE ROLE SAP_ROLE;
---GRANT CREATE DATABASE ON ACCOUNT TO ROLE SAP_ROLE; --grant so the role can creat the database.
---GRANT CREATE INTEGRATION ON ACCOUNT TO ROLE SAP_ROLE; --grant to create the integration between Snowflake and AWS S3.
---GRANT ROLE SAP_ROLE to ROLE accountadmin; --ASSIGN ROLE TO an existing role that your user has.
---USE ROLE SAP_ROLE;
 
---Use the following to set up and validate the integration between Snowflake and S3 buckets. Check out the documentation on how to get the ARN and external ID.
-CREATE OR REPLACE STORAGE INTEGRATION SAP_INTEGRATION --more infor here https://docs.snowflake.com/en/user-guide/data-load-s3-config-storage-integration
+--Use the following to set up and validate integration between Snowflake and S3 buckets.
+
+/*
+The following step is specific to your deployment with AWS S3. It is important so the SNS protocol can alert Snowflake that new files have landed
+in the S3 bucket, so Snowflake can pick them up automatically. 
+Read the following documentation to set this up correctly.
+https://docs.snowflake.com/en/user-guide/data-load-s3-config-storage-integration
+
+ */
+
+CREATE OR REPLACE STORAGE INTEGRATION SAP_INTEGRATION 
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
   ENABLED = TRUE
@@ -29,32 +32,40 @@ CREATE OR REPLACE STORAGE INTEGRATION SAP_INTEGRATION --more infor here https://
   STORAGE_AWS_EXTERNAL_ID = 'EU_DEMO47_SFCRole=3571_xQgnqahg8fymSujM2bXWqAWHwOM=' --so you don't have to set it each time your recreate the storage integration.
   STORAGE_ALLOWED_LOCATIONS = ('s3://sap-s3-raw/');
 
---Validate the storage integration
---SHOW  STORAGE INTEGRATIONS;
---DESC INTEGRATION SAP_INTEGRATION;
---SELECT SYSTEM$VALIDATE_STORAGE_INTEGRATION ('SAP_INTEGRATION', 's3://sap-s3-raw/', 'validate_all.txt', 'all');
+/*  Use the following commands to Validate the storage integration */
+SHOW  STORAGE INTEGRATIONS;
+DESC INTEGRATION SAP_INTEGRATION;
+SELECT SYSTEM$VALIDATE_STORAGE_INTEGRATION ('SAP_INTEGRATION', 's3://sap-s3-raw/', 'validate_all.txt', 'all');
 
---Create a database and schema to land the raw data from SA 
-CREATE or REPLACE DATABASE SAP_SNOW COMMENT = 'For S4 raw data';
+/* Create the database and schema that will be used to land the raw data from SAP */
+CREATE or REPLACE DATABASE SAP_SNOW COMMENT = 'For SAP data';
 USE DATABASE SAP_SNOW; 
 CREATE OR REPLACE SCHEMA SAP_RAW; 
 USE SCHEMA  SAP_RAW;
---Create file format so Snowflake knows that the SAP files are coming into the S3 bucket as parquet files.
+
+-- Create file format so Snowflake knows that the SAP files are coming into the S3 bucket as parquet files.
 CREATE OR REPLACE FILE FORMAT SAP_FILE TYPE = parquet COMMENT = 'Parquet file format for AWS Glue files landed in S3';
 
---create a Snowflake stage to point to the S3 bucket.
+-- Create a Snowflake stage to point to the S3 bucket.
 CREATE OR REPLACE STAGE SAP_STAGE
     URL = $SAPS3TARGETURL
     STORAGE_INTEGRATION = SAP_INTEGRATION
     FILE_FORMAT = SAP_FILE;
 
-  --Test integration
-  --run the job from AWS glue to get files to work with. Check out the cloud formation template to get this. 
- --SHOW stages; LIST @SAP_STAGE/fi_ar_4/; LIST @SAP_STAGE/customer; LIST @SAP_STAGE/material/;
+/*
+If you have not already, run the job from AWS glue to land the files. Check out the cloud formation template to get this. 
+Use the following command afterwards to verify the files landed in the bucket.
+*/
+SHOW stages; LIST @SAP_STAGE/fi_ar_4/; LIST @SAP_STAGE/customer; LIST @SAP_STAGE/material/;
 
---Let's use Snowflake's inference capability to build up the table definitions for the raw layer.
+/* 
 
- CREATE OR REPLACE TABLE FI_AR_4 --change to "0fi_ar_4"
+Setting up the table definitions. 
+Using infer_schema is an easy way to set up the table definitions. It sources from the parquet file definition to do this. 
+
+ */
+
+ CREATE OR REPLACE TABLE FI_AR_4
   USING TEMPLATE (
     SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
       FROM TABLE(
@@ -63,8 +74,6 @@ CREATE OR REPLACE STAGE SAP_STAGE
       , FILE_FORMAT=>'SAP_FILE'
         )
       ));
-
---SELECT get_ddl('TABLE', 'Z_FI_AR_4');
 
   CREATE OR REPLACE TABLE CUSTOMER
   USING TEMPLATE (
@@ -76,8 +85,6 @@ CREATE OR REPLACE STAGE SAP_STAGE
         )
       ));
 
-SELECT get_ddl('TABLE', 'CUSTOMER');
-
 CREATE OR REPLACE TABLE MATERIAL
   USING TEMPLATE (
     SELECT ARRAY_AGG(OBJECT_CONSTRUCT(*))
@@ -88,16 +95,22 @@ CREATE OR REPLACE TABLE MATERIAL
         )
       ));
 
---SELECT get_ddl('TABLE', 'MATERIAL');
+/*Check out the table definitions */
+SELECT get_ddl('TABLE', 'Z_FI_AR_4'); SELECT get_ddl('TABLE', 'CUSTOMER'); SELECT get_ddl('TABLE', 'MATERIAL');
 
+/* 
 
---!!now delete the files from S3!!
-REMOVE @sap_stage/fi_ar_4/;
-REMOVE @sap_stage/customer/;
-REMOVE @sap_stage/material/;
+Now that we have built the table definition automatically, let's delete the files from the S3 bucket, make sure our raw tables are empty, and then create the pipes to monitor the bucket.
+Once the pipes are running, we will run the AWS Glue job again to populate the buckets, snowpipe will detect new files because of the AWS SNS integration, and will automatically load the data into Snowflake.
+
+*/
+REMOVE @sap_stage/fi_ar_4/; REMOVE @sap_stage/customer/; REMOVE @sap_stage/material/;
 --truncate tables
 TRUNCATE TABLE FI_AR_4; TRUNCATE TABLE CUSTOMER; TRUNCATE TABLE MATERIAL;
 
+/* 
+Ok, now let's create the pipes. They will start automatically.
+ */
 CREATE OR REPLACE PIPE FI_AR_4_PIPE
 AUTO_INGEST = TRUE
 AS
@@ -122,27 +135,35 @@ AS
   MATCH_BY_COLUMN_NAME='CASE_INSENSITIVE'
   ON_ERROR=CONTINUE; 
 
-SHOW pipes; -- get the ARN and configure the SNS for the S3 bucket from the AWS console
-/*arn:aws:sqs:us-west-2:227202484309:sf-snowpipe-AIDATJZSSZBKXUQTZVL4W-j9b6KZYM9aqg3IToJ1ngqQ */
+/* Check that the pipes were created and are running */
+SHOW pipes; 
 SELECT SYSTEM$PIPE_STATUS ('FI_AR_4_PIPE'); SELECT SYSTEM$PIPE_STATUS ('CUSTOMER_PIPE'); SELECT SYSTEM$PIPE_STATUS ('MATERIAL_PIPE');
   --ALTER PIPE Z_BW_ODATA_0FI_AR_4_1_SRV_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
   
 /*
 
-RE-RUN  THE AWS GLUE JOB NOW! The pipes need to be in a running state.
+Now, re-run  the AWS glue job from the AWS console! The pipes need to be in a running state.
 
 */
 
+
+/* Once the AWS Glue job has completed you can check if the files landed in the S3 bucket by checking the stages */
 LIST @SAP_STAGE/fi_ar_4/; LIST @SAP_STAGE/customer/; LIST @SAP_STAGE/material/;
 
+/* After 1-2 minutes after the AWS job has run, check that the data has landed into the Snowflake tables */
 SELECT * FROM FI_AR_4; SELECT * FROM CUSTOMER; SELECT *  FROM MATERIAL; --2701
 
----TO PAUSE!!!!!!!!
-ALTER PIPE FI_AR_4_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
-ALTER PIPE CUSTOMER_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
-ALTER PIPE MATERIAL_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+/*  
 
---Tear down
---DROP DATABASE   SAP_SNOW;
+Once the data has loaded into the tables, you can pause the pipes to reduce costs. Pause them by uncommenting the following lines.
+
+*/
+
+--ALTER PIPE FI_AR_4_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+--ALTER PIPE CUSTOMER_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+--ALTER PIPE MATERIAL_PIPE SET PIPE_EXECUTION_PAUSED = TRUE; --make sure to turn off pipe so you don't pay.
+
+/* Congrats! You've now loaded the SAP data into the raw zone. Now time to build out the harmonization and reporting layer. Go to the Python worksheet */
+
 
 
